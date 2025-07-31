fe13e8e31bc166d4b7994017399ee9e3ee5dce32
uploadimg <imagegithub@gmail.com>
2025-07-31 07:39:36 +0700

    ssssssssssss

---
+++ file/1753922376057_jackmau.js
@@ -0,0 +1,69 @@
+var chonkieu = [
+{
+	anhnen:"https://lh3.googleusercontent.com/-5w1F4L8Caao/YerZbyUZBtI/AAAAAAAAAxo/1Jud5LNY-AYjDXvLvVb1GJY0m56zl722QCNcBGAsYHQ/s0/jack1.png",
+	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+    
+{
+	anhnen:"https://lh3.googleusercontent.com/-Ydv2tQdBduA/Yerbh0ZcHQI/AAAAAAAAAxw/g8XOAW8Gh0oXZ0d_k_-s0grLiLoEKmUMACNcBGAsYHQ/s0/jack2.png",
+ 	bgcolor:"#474054",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+	{
+	anhnen:"https://lh3.googleusercontent.com/-jbWkpbQmmFE/Ye7I-x9iZcI/AAAAAAAAAyk/96V_aAkhvAgLBAf9eSG6yASfutCML1hrACNcBGAsYHQ/s0/jack3.png",
+ 	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+	
+	
+		{
+	anhnen:"https://lh3.googleusercontent.com/-mDAtxT1XfNE/Ye7I--DNbrI/AAAAAAAAAyg/S4TCNX-GAT0W2mWabllR-or0Uv0QzrYXwCNcBGAsYHQ/s0/jack4.png",
+ 	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+		{
+	anhnen:"https://lh3.googleusercontent.com/-jyh-V0vyUvM/Ye7I_NmTMnI/AAAAAAAAAyo/PzYDxXKCSIQp9ABSe8qdmTbyVZUi_W-KwCNcBGAsYHQ/s0/jack5.png",
+ 	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+	{
+	anhnen:"https://lh3.googleusercontent.com/-zHneLkxmpw4/Ye7JANTmJbI/AAAAAAAAAys/z-WTrzeeqtEHc979uswDQv0f7NTGIT41gCNcBGAsYHQ/s0/jack6.png",
+ 	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+		{
+	anhnen:"https://lh3.googleusercontent.com/-40iPf1W2LD4/Ye7JAsyQk0I/AAAAAAAAAyw/1UjXEeWOKMAv7sxdB0Y1XsHq9mSHYfERQCNcBGAsYHQ/s0/jack7.png",
+ 	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+			{
+	anhnen:"https://lh3.googleusercontent.com/-NIo68WYu6Zo/Ye7JAz7frvI/AAAAAAAAAy0/-VgoAFGmSK88WQEJuiIQSw1ZOemnH9zuQCNcBGAsYHQ/s0/jack8.png",
+ 	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	},
+
+{
+	anhnen:"https://lh3.googleusercontent.com/-bFqmpgK04MY/Ye7JBMyNIAI/AAAAAAAAAy4/H2CZgRORRSkQItOZ2PdBX9WhgJA6ivRewCNcBGAsYHQ/s0/jack9.png",
+ 	bgcolor:"#493669",
+	chieuronganhxuat:"900",
+	chieucaoanhxuat:"900"
+		
+	}
+];
